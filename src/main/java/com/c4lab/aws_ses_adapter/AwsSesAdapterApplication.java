f26730008515be29bcda53d2e498e9cd2c0692aa
sok-hs <hengsok2202@gmail.com>
2026-09-05 12:25:44 +0700

    feat: first commit

---
+++ src/main/java/com/c4lab/aws_ses_adapter/AwsSesAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.c4lab.aws_ses_adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSesAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSesAdapterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/c4lab/aws_ses_adapter/AwsSesAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c4lab.aws_ses_adapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSesAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
